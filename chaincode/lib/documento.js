fe6c4a7f2e5873c8435155abb0dbd3f76205070d
Jonathan Angulo <jhonatanrojass23@gmail.com>
2024-10-10 17:52:00 -0500

    Agregar el Campo hashDocumento a la Clase Documento

--- chaincode/lib/documento.js
+++ chaincode/lib/documento.js
@@ -1,13 +1,14 @@
 'use strict';
 
 class Documento {
-    constructor(id, idCaso, tipoDocumento, nombreArchivo, fechaSubida, usuarioResponsable) {
+    constructor(id, idCaso, tipoDocumento, nombreArchivo, fechaSubida, usuarioResponsable, hashDocumento) {
         this.id = id;
         this.idCaso = idCaso;
         this.tipoDocumento = tipoDocumento;
         this.nombreArchivo = nombreArchivo;
         this.fechaSubida = fechaSubida;
         this.usuarioResponsable = usuarioResponsable;
+        this.hashDocumento = hashDocumento;
     }
 }
 
